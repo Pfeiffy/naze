55e7855d76bacb02ef76a8c036316a767a2c0837
Papa <Papa@Marius-PC.Speedport_W_921V_1_39_000>
2016-09-23 20:15:54 +0200

    ee

--- Naze/src/SeriellTest.java
+++ Naze/src/SeriellTest.java
@@ -21,16 +21,13 @@ public class SeriellTest implements SerialPortEventListener {
 	static String temp2;
 	public static int msp = 108;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SeriellTest st = new SeriellTest();
 
-
-		st.schreibeAufSeriell();
-
-		//
-		// st.schreibeAufSeriell("0");
-		// st.schreibeAufSeriell("100");
-		// st.schreibeAufSeriell("100");
+		for (int x = 1; x < 100; x++) {
+			st.schreibeAufSeriell();
+			Thread.sleep(200);
+		}
 
 	}
 
@@ -52,7 +49,6 @@ public class SeriellTest implements SerialPortEventListener {
 		}
 	}
 
-	// byte[] response = new byte[length];
 	public void serialEvent(SerialPortEvent evt) {
 		if (evt.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
 			try {
@@ -60,75 +56,37 @@ public class SeriellTest implements SerialPortEventListener {
 
 				byte[] response = new byte[length];
 
-				int zaehler=0;
+				int zaehler = 0;
 				int intArray[] = new int[length];
 				for (int i = 0; i < length && inputStream.available() > 0; i++) {
 					int value = inputStream.read();
 					response[i] = (byte) value;
-					System.out.println("Value: "+ i+ " bi: "+ Integer.toBinaryString(value)+ " int: " + value+ " byte: " + response[i]);
+//					System.out.println("Value: " + i + " bi: " + Integer.toBinaryString(value) + " int: " + value
+//							+ " byte: " + response[i]);
 					intArray[i] = value;
 					zaehler++;
 				}
 				System.out.println("------------------");
-				for (int x = 4; x < zaehler; x++) {
-					System.out.println("response: " + x + " " + response[x]);
-					String bi = Integer.toBinaryString((int) response[x]);
-					// System.out.println(bi);
-				}
+
 				int pointer = 4;
 
-				int angx=0;
-				int angy=0;
-				int heading=0;
-				
-				angx =  intArray[4];
-				angx += response[5] * 256;
-				
-				angy =  intArray[6];
-				angy += response[7] * 256;
-				
-				heading =  intArray[8];
-				heading += response[9] * 256;
-				
+				int angx = 0;
+				int angy = 0;
+				int heading = 0;
 
-				
-				
-//				int angx = response[pointer];
-//				pointer += 2;
-//				int angy = (response[pointer++] & 0xFF) | ((response[pointer++] & 0xFF) << 8);
-//
-//				int heading = (response[pointer++] & 0xFF) | ((response[pointer++] & 0xFF) << 8);
+				angx = intArray[4];
+				angx += response[5] * 256;
+
+				angy = intArray[6];
+				angy += response[7] * 256;
+
+				heading = intArray[8];
+				heading += response[9] * 256;
 
 				System.out.println("angx: " + angx + " angy: " + angy + " heading: " + heading);
 
-				// int c;
-				// // lies bis \r\n
-				// ArrayList<Integer> aList = new ArrayList<Integer>();
-				// while ((c = inputStream.read()) != -1) {
-				// System.out.print(c);
-				// System.out.print(" ");
-				// System.out.println(Character.toString((char) c));
-				// if (c != 13) {
-				// // System.out.println("empfange");
-				// readBuffer.append((char) c);
-				// aList.add(c);
-				// }
-				// }
-				// inputStream.close();
-				//
-				// // MSP_ATTITUDE 109
-				// if (aList.get(4) == msp) {
-				//
-				// // String angx = Integer.toBinaryString(aList.get(5)) +
-				// // Integer.toBinaryString(aList.get(6));
-				// System.out.println("angx: "
-				// + int8toint16(aList.get(6), aList.get(5)));
-				// }
+//				System.exit(0);
 
-				System.exit(0);
-				// empfangen = readBuffer.toString();
-
-				// mach was mit den eingelesenen Daten
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -168,14 +126,6 @@ public class SeriellTest implements SerialPortEventListener {
 		}
 	}
 
-	private static int int8toint16(int loByte, int hiByte) {
-		String high = String.format("%08d", Integer.parseInt(Integer.toBinaryString(hiByte)));
-		String low = String.format("%08d", Integer.parseInt(Integer.toBinaryString(loByte)));
 
-		String bi16 = high + low;
-		int int16 = Integer.parseInt(bi16, 2);
-		System.out.println(bi16);
-		return int16;
-	}
 
 }
