2355fda6b7e326bfba2582186c640d8db6e3493d
Papa <Papa@Marius-PC.Speedport_W_921V_1_39_000>
2016-09-29 18:05:24 +0200

    ll

--- Naze/src/NazeCop.java
+++ Naze/src/NazeCop.java
@@ -1,5 +1,6 @@
 import gnu.io.*;
 
+import java.awt.SecondaryLoop;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -11,8 +12,8 @@ public class NazeCop implements SerialPortEventListener {
 	private CommPortIdentifier portID;
 	private SerialPort serport;
 	private InputStream inputStream;
-	private OutputStream outputStream;
-	static String port = "COM4";
+	private static OutputStream outputStream;
+	static String port = "COM21";
 	static String empfangen;
 	static String temp1;
 	static String temp2;
@@ -33,94 +34,81 @@ public class NazeCop implements SerialPortEventListener {
 	static int MSP_SET_RC_TUNING = 204;
 	static int MSP_SET_MISC = 207;
 
-	public static void main(String[] args) throws InterruptedException,
-			IOException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		NazeCop st = new NazeCop();
 
-		st.sendeBefehlMSP_SET_RC_TUNING();
-		Thread.sleep(40);
-		st.sendeAbfrageNaze(MSP_RC_TUNING);
+		//sendeBefehlArray();
+		//st.sendeBefehlMSP_SET_RC_TUNING();
+		//Thread.sleep(40);
+		st.sendeAbfrageNaze(MSP_ATTITUDE);
+
+	}
+
+	private static void sendeBefehlArray() throws IOException {
+
+//		int befehlArray[] = { 36, 77, 60, 11, 204, 177, 100, 33, 44, 55, 66, 52, 100, 220, 5, 84, 183, 36, 77, 60, 0,
+//				105, 105 };
+		int befehlArray[] = { 36, 77, 60, 11, 204, 177, 100, 33, 44, 55, 66, 52, 100, 220, 5, 84, 183 };
+
+		for (int x = 0; x < befehlArray.length; x++) {
+			outputStream.write(befehlArray[x]);
+		}
+
+		outputStream.flush();
+		outputStream.close();
 
 	}
 
 	private void sendeBefehlMSP_SET_RC_TUNING() throws IOException {
 
-		int datalengt = 7;
+		int datalengt = 11;
 
 		int RC_Rate = 11;
 		int RC_EXPO = 12;
-		int RollPitchRate = 13;
-		int YawRate = 14;
-		int DynThrPID = 15;
+		int Roll_Rate = 13;
+		int Pitch_Rate = 13;
+		int Yaw_Rate = 14;
+		int TPA = 15;
 		int Throttle_MID = 16;
 		int Throttle_EXPO = 17;
+		int TPA_BR_1 = 220;
+		int TPA_BR_2 = 5;
+		int RC_Yaw__EXPO = 17;
 
-		int[] arrChecksum = { datalengt, MSP_SET_RC_TUNING, RC_Rate, RC_EXPO,
-				RollPitchRate, YawRate, DynThrPID, Throttle_MID, Throttle_EXPO };
+		int[] arrChecksum = { datalengt, MSP_SET_RC_TUNING, RC_Rate, RC_EXPO,Roll_Rate,Pitch_Rate,Yaw_Rate,TPA,Throttle_MID,Throttle_EXPO,TPA_BR_1,TPA_BR_2,RC_Yaw__EXPO};
 		int checksum = getChecksum(arrChecksum);
 
-		// sendenHeaderBefehl();
-		// outputStream.write(7);
-		// outputStream.write(108);
-		// outputStream.write(11);
-		// outputStream.write(12);
-		// outputStream.write(13);
-		// outputStream.write(14);
-		// outputStream.write(15);
-		// outputStream.write(16);
-		// outputStream.write(17);
-		// outputStream.write(checksum);
-		//
 		sendenHeaderBefehl();
 		outputStream.write(datalengt);
 		outputStream.write(MSP_SET_RC_TUNING);
 		outputStream.write(RC_Rate);
 		outputStream.write(RC_EXPO);
-		outputStream.write(RollPitchRate);
-		outputStream.write(YawRate);
-		outputStream.write(DynThrPID);
+		outputStream.write(Roll_Rate);
+		outputStream.write(Pitch_Rate);
+		outputStream.write(Yaw_Rate);
+		outputStream.write(TPA);
 		outputStream.write(Throttle_MID);
 		outputStream.write(Throttle_EXPO);
+		outputStream.write(TPA_BR_1);
+		outputStream.write(TPA_BR_2);
+		outputStream.write(RC_Yaw__EXPO);
 		outputStream.write(checksum);
-		//
-		//
-		//
-		//
-		// 6 6c ff ff 3 0 70 0 19
-		// 6 108 255 255 3 0 112 0 25
-
-		// sendenHeaderBefehl();
-		// outputStream.write(6);
-		// outputStream.write(108);
-		// outputStream.write(255);
-		// outputStream.write(255);
-		// outputStream.write(3);
-		// outputStream.write(0);
-		// outputStream.write(112);
-		// outputStream.write(0);
-		// outputStream.write(25);
-
-		// int []arrChecksum = {6,108,255,255,3,0,112,0};
-		// int checksum = getChecksum(arrChecksum);
 
 		outputStream.flush();
 		outputStream.close();
 
-		// System.exit(0);
 
 	}
 
 	public NazeCop() {
 		try {
 			portID = CommPortIdentifier.getPortIdentifier(port); // oder
-			System.out.println(CommPortIdentifier.getPortIdentifier(port)
-					.isCurrentlyOwned());
+			System.out.println(CommPortIdentifier.getPortIdentifier(port).isCurrentlyOwned());
 
 			// // /dev/ttyS0
 			serport = (SerialPort) portID.open("foobar", 2000);
 
-			serport.setSerialPortParams(115200, SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			serport.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 			inputStream = serport.getInputStream();
 			outputStream = serport.getOutputStream();
 			serport.addEventListener(this);
@@ -142,8 +130,7 @@ public class NazeCop implements SerialPortEventListener {
 				for (int i = 0; i < length && inputStream.available() > 0; i++) {
 					int value = inputStream.read();
 					response[i] = (byte) value;
-					System.out.println("Value: " + i + " bi: "
-							+ Integer.toBinaryString(value) + " int: " + value
+					System.out.println("Value: " + i + " bi: " + Integer.toBinaryString(value) + " int: " + value
 							+ " byte: " + response[i]);
 					intArray[i] = value;
 					zaehler++;
@@ -162,7 +149,7 @@ public class NazeCop implements SerialPortEventListener {
 					getMisc(response, intArray);
 				}
 
-				//System.exit(0);
+				System.exit(0);
 
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -191,7 +178,6 @@ public class NazeCop implements SerialPortEventListener {
 	}
 
 	// Lageposition und Kompasswinkel
-
 	private void getAttitude(byte[] response, int[] intArray) {
 
 		angx = uint8to16(response, 4);
@@ -200,11 +186,10 @@ public class NazeCop implements SerialPortEventListener {
 
 		heading = uint8to16(response, 8);
 
-		System.out.println("angx: " + angx + " angy: " + angy + " heading: "
-				+ heading);
+		System.out.println("angx: " + angx + " angy: " + angy + " heading: " + heading);
 	}
 
-	// Hï¿½he
+	// Höhe
 	private void getAltitude(byte[] response, int[] intArray) {
 
 		altitude = uint8to32(intArray, 4);
@@ -225,8 +210,7 @@ public class NazeCop implements SerialPortEventListener {
 
 	private int uint8to16(byte[] byteArray, int startPointer) {
 
-		ByteBuffer b = ByteBuffer.wrap(byteArray, startPointer, 2).order(
-				ByteOrder.LITTLE_ENDIAN);
+		ByteBuffer b = ByteBuffer.wrap(byteArray, startPointer, 2).order(ByteOrder.LITTLE_ENDIAN);
 		int wert16 = b.getShort();
 
 		return wert16;
@@ -234,8 +218,8 @@ public class NazeCop implements SerialPortEventListener {
 
 	private int uint8to32(int[] intArray, int startPointer) {
 
-		int wert32 = (intArray[startPointer] + intArray[startPointer + 1] * 256
-				+ intArray[startPointer + 2] * 256 + intArray[startPointer + 3] * 256);
+		int wert32 = (intArray[startPointer] + intArray[startPointer + 1] * 256 + intArray[startPointer + 2] * 256
+				+ intArray[startPointer + 3] * 256);
 		if (intArray[startPointer] >= 128) {
 			wert32 = 256 * 256 - wert32;
 		}
@@ -246,42 +230,29 @@ public class NazeCop implements SerialPortEventListener {
 
 	private void sendeAbfrageNaze(int befehl) {
 		try {
-			// // 24 4d 3c 00 01 01
-			// outputStream.write(0x24);//36
-			// outputStream.write(0x4d);//77
-			// outputStream.write(0x3c);
-			// outputStream.write(0x00);
-			// outputStream.write(0x01);
-			// outputStream.write(0x01);
 
-			// // 24 4d 3c 00 01 01
-			// outputStream.write(0x24);
-			// outputStream.write(0x4d);
-			// outputStream.write(0x3c);
-			// outputStream.write(0x00);
-			// outputStream.write(0x6c);
-			// outputStream.write(0x6c);
-
-			// 24 4d 3c 00 01 01
 			sendenHeader();
 			outputStream.write(befehl);
 			outputStream.write(befehl);
 
 			outputStream.flush();
 			outputStream.close();
+
+			// in Eeprom schreiben
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void sendenHeader() throws IOException {
-		outputStream.write(36);
-		outputStream.write(77);
-		outputStream.write(60);
-		outputStream.write(0);
+	public static void sendenHeader() throws IOException {
+		outputStream.write(36); // $
+		outputStream.write(77); // M
+		outputStream.write(60); // >
+		outputStream.write(0); // 0
 	}
 
-	private void sendenHeaderBefehl() throws IOException {
+	public static void sendenHeaderBefehl() throws IOException {
 		outputStream.write(36);
 		outputStream.write(77);
 		outputStream.write(60);
@@ -295,5 +266,6 @@ public class NazeCop implements SerialPortEventListener {
 		}
 		return checksum;
 	}
+
 
 }
--- Naze/src/test.java
+++ Naze/src/test.java
@@ -1,17 +1,43 @@
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		byte hiByte = 82;
-		byte loByte = 1;
+		// int befehlArray[] = { 36, 77, 60, 11, 204, 177, 100, 33, 44, 55, 66,
+		// 52, 100, 220, 5, 84, 183, 36, 77, 60, 0,
+		// 105, 105 };
+		int befehlArray[] = { 36, 77, 60, 11, 204, 177, 100, 33, 44, 55, 66, 52, 100, 220, 5, 84, 183 };
+		int erg = getChecksum(befehlArray);
+		System.out.println(erg);
 
-	int ss = (int)	((hiByte & 0xFF) | (loByte & 0xFF) << 8);
-	
-	
-		System.out.println(ss);
-		
-		
+		byte[] byteArray = new byte[] { (byte) 240, (byte) 5 };
+
+		ByteBuffer b = ByteBuffer.wrap(byteArray).order(ByteOrder.LITTLE_ENDIAN);
+		int wert16 = b.getShort();
+
+		System.out.println("Wert16: " + wert16);
+
+	}
+
+	private static int getChecksum(int[] intArray) {
+		int checksum = 0;
+		for (int x = 3; x < intArray.length - 1; x++) {
+			checksum ^= intArray[x];
+		}
+		return checksum;
+	}
+
+	private int uint8to16() {
+
+		byte[] byteArray = new byte[] { (byte) 7, (byte) 7 };
+
+		ByteBuffer b = ByteBuffer.wrap(byteArray).order(ByteOrder.LITTLE_ENDIAN);
+		int wert16 = b.getShort();
+
+		return wert16;
 	}
 
 }
